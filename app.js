0ac8e7e3126610893be168725165cd9bf97d9a06
Marissa Belombre <mbelo@MJBGALAXY>
2022-02-01 02:48:40 -0800

    edited js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+
+
+
 const printProfileData = (profileDataArr) => {
 	// This...
 	for (let i = 0; i < profileDataArr.length; i += 1) {
@@ -8,5 +12,4 @@ const printProfileData = (profileDataArr) => {
 
 	// Is the same as this...
 	profileDataArr.forEach(profileItem => console.log(profileItem));
-	});
-};
+	};
